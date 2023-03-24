e689201f4ba5dda008167a1ae9c5d89c43f62f34
李博 <2565477149@qq.com>
2023-03-24 15:45:00 +0800

    init

---
+++ src/main/java/com/shanhai/springbootinittemplate/SpringbootInitTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.shanhai.springbootinittemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInitTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootInitTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shanhai/springbootinittemplate/SpringbootInitTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanhai.springbootinittemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInitTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
